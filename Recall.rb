cbd418db31fc646bcaf0c405b5184a9ae763c065
FaisalFehad <FaisalFehad@gmail.com>
2016-05-14 12:40:37 +0000

    Add delete and edit + erb views

--- Recall.rb
+++ Recall.rb
@@ -19,21 +19,54 @@ end
 DataMapper.auto_upgrade!
 
 
-  get '/' do
-    @notes = Note.all :order => :id.desc
-    @title = "All Notes"
-    haml :index
-  end
+get '/' do
+  @notes = Note.all :order => :id.desc
+  @title = "All Notes"
+  haml :index
+end
 
-  post '/' do
-    n = Note.new
-    n.content = params[:content]
-    n.created_at = Time.now
-    n.updated_at = Time.now
-    n.save
-      redirect '/'
-  end
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+    redirect '/'
+end
 
-  not_found do
-   halt 404, "Page not found"
-  end
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params:[:id]}"
+  erb :edit
+end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of note ##{params[:id]}"
+  erb :delete
+end
+
+delete '/:id' do
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
+
+delete '/:id' do
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
+
+not_found do
+  halt 404, "Page not found"
+end
